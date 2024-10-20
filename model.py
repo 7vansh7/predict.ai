f682305f1912df7265ab6a6ec2e3ad5f33e856a9
vansh <115992070+7vansh7@users.noreply.github.com>
2024-10-20 09:56:27 +0000

    .

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
 safety_settings = [
@@ -22,7 +23,7 @@ safety_settings = [
   }
 ]
 
-genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+genai.configure(api_key=st.secrets['GOOGLE_API_KEY'])
 llm = genai.GenerativeModel(model_name='gemini-1.5-pro-002',safety_settings=safety_settings)
 history = []
 chat = llm.start_chat(history=history)
