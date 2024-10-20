0bcca0934c658c48fa24a3ca00db76d6c6d74c60
vansh <115992070+7vansh7@users.noreply.github.com>
2024-10-20 13:23:31 +0000

    .

--- model.py
+++ model.py
@@ -27,3 +27,15 @@ genai.configure(api_key='AIzaSyA7eoFGIqKaCA7oUYMqzT83NexlDnRjYfs')
 llm = genai.GenerativeModel(model_name='gemini-1.5-pro-002',safety_settings=safety_settings)
 history = []
 chat = llm.start_chat(history=history)
+psy_prompt = """You name is Aura, you are a psychologist, an expert in 
+predicting human behaviour.
+You are adept in psychology, philosophy, and literature.
+Ask questions from the user to better your analysis and answer.
+Give examples from contemporary studies if required.
+You give people psychological help and also help them predicting other 
+people's behaviour.
+Give analysis in tabular format with the probability of things happening if required.
+Talk like a HUMAN PSYCHOLOGIST.
+Give a good initial response 
+"""
+initial_response = chat.send_message(psy_prompt).text
